d01ea554978b748f7ad60a743684885b719b81df
ACottrell <ultimatereality92@gmail.com>
2015-02-15 20:23:12 +0000

    Wutno author:
    clean up ultimations mess with using spaces instead of tabs and add "epic memes" for secondary domains as well as corrected something that's been bothering me...

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*  PHP System Status
  *  ------------------------------------------
  *  Authors: wutno (#/g/tv - Rizon), ed (https://github.com/9001)
- *  Last update: 5/20/2014 12:04AM -5GMT (add core temperature support, requires lm_sensors, and had to remove 1 output otherwise 1080 will have scroll bar.)
+ *  Last update: 2/15/2015 3:11AM -5GMT (clean up ultimations mess with using spaces instead of tabs and add "epic memes" for secondary domains as well as corrected something that's been bothering me...)
  *
  *
  *  GNU License Agreement
@@ -23,7 +23,7 @@
  *  http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-#Since we all enjoy Open Source
+#Since we all enjoy FOSS
 if(isset($_GET['dat']) && $_GET['dat'] == "sauce"){
 	$lines = implode(range(1, count(file(__FILE__))), '<br />');
 	$content = highlight_file(__FILE__, TRUE);
@@ -49,13 +49,13 @@ function calculate_percentage($used, $total){
 
 $uptime = exec('uptime');
 preg_match('/ (.+) up (.+) user(.+): (.+)/', $uptime, $update_out);
-$users_out = (substr($update_out[2], strrpos($update_out[2], ' ')+1))-1;
+$users_out = (substr($update_out[2], strrpos($update_out[2], ' ')+1));
 $uptime_out = substr($update_out[2], 0, strrpos($update_out[2], ' ')-2);
 $load_out = str_replace(', ',', <small>',$update_out[4]).str_repeat('</small>',2);
 
 #hdd info
 $raiddrive = explode(' ', preg_replace('/\s\s+/', ' ', exec('df /dev/md0')));
-$raiddrive_out = format_bytes(kb2bytes($raiddrive[2])).'<small> / '.format_bytes(kb2bytes($raiddrive[1])).' <small>('.calculate_percentage(kb2bytes($raiddrive[2]), kb2bytes($raiddrive[1])).'% Free)</small></small>';
+$raiddrive_out = format_bytes(kb2bytes($raiddrive[2])).'<small> / '.format_bytes(kb2bytes($raiddrive[1])).' <small>('.calculate_percentage(kb2bytes($raiddrive[2]), kb2bytes($raiddrive[1])).'% free)</small></small>';
 
 #bandwidth info
 #if we have this enabled creates a scroll bar for 1080
@@ -87,20 +87,20 @@ $memory['Used Swap'] = $memory['Total Swap'] - $memory['Free Swap'];
 $memory['Used RAM'] = $memory['Total RAM'] - $memory['Free RAM'] - $memory['Cached RAM'];
 $memory['RAM Percent Free'] = calculate_percentage($memory['Used RAM'],$memory['Total RAM']);
 $memory['Swap Percent Free'] = calculate_percentage($memory['Used Swap'],$memory['Total Swap']);
-$memory_out = format_bytes($memory['Used RAM']).'<small> / '.format_bytes($memory['Total RAM']).' <small> *'.format_bytes($memory['Cached RAM']).' Cached ('.$memory['RAM Percent Free'].'% Free)</small></small>';
-$swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory['Total Swap']).' <small>('.$memory['Swap Percent Free'].'% Free)</small></small>';
+$memory_out = format_bytes($memory['Used RAM']).'<small> / '.format_bytes($memory['Total RAM']).' <small> *'.format_bytes($memory['Cached RAM']).' Cached ('.$memory['RAM Percent Free'].'% free)</small></small>';
+$swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory['Total Swap']).' <small>('.$memory['Swap Percent Free'].'% free)</small></small>';
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>NoizeBox Stats</title>
+		<title>NoizeBox stats</title>
 		<meta name="robots" content="noindex, nofollow, noarchive, nosnippet, noodp" /> <!-- I fucking hate robots... -->
 		<meta name="description" content="Status page for NoizeBox." />
 		<meta http-equiv="refresh" content="10">
 		<meta charset="UTF-8" />
 		<style type="text/css">
 			span { color: #FFB;display: block;font-size: 1.3em;margin-bottom: .5em;padding: 0 .5em; }
-			html { background: #000 url('images/bg.jpg'); background-size:cover; background-attachment:fixed; color: #FFF;font-family: sans-serif; font-size: 1.8em;padding: 1em 2em; }
+			html { background: #000 url('images/bg.png'); background-size:cover; background-attachment:fixed; color: #FFF;font-family: sans-serif; font-size: 1.8em;padding: 1em 2em; }
 			div { float: right;text-align: right; }
 			a { color: #FF6;display: block;font-size: 1.7em;text-decoration: none; }
 			small { color: #FF9; }
@@ -109,26 +109,30 @@ $swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory
 	</head>
 	<body>
 		<div id="links">
-		bros 
-			<a href="https://www.flickr.com/photos/ultimation/">ultimation</a>
-			<a href="/~wutno/">wutno</a>	
+		bros
+			<a href="/~ultimation/">ultimation</a>
+			<a href="/~dawnkiller/">dawnkiller</a>
+			<a href="/~jackperazzi/">jack</a>
+			<a href="/~wutno/">wutno</a>
 			<a href="/~psychonaught/">psychonaught</a>
-			<a href="/~hash/">hash</a>
-			<br/>
+
 		links
+			<a href="ts3server://noize.link?port=9987&password=hunter2&addbookmark=noize.link">teamspeak</a>
 			<a href="http://noize.link:8080">minecraft</a>
-			<a href="http://192.168.0.22:32400/manage/index.html">plex<small><small><small> (local only)</small></small></small></a>
+			<a href="/plex/index.html">plex<small><small><small> listings</small></small></small></a>
+			<a href="http://192.168.1.10:32400/manage/index.html">plex<small><small><small> (local only)</small></small></small></a>
 			<a href="index.php?dat=sauce">source</a>
-		</div>
-		local time<span><?=date("Y-F-j H:i:s", time());?></span> <!-- Server time is acutally PHP time since PHP wants to bitch -->
-		uptime<span><?=$uptime_out;?></span> <!-- Server uptime --> 
+    </div>
+		local time<span><?=date("Y-F-j H:i:s", time());?></span> <!-- Server time is actally PHP time since PHP wants to bitch -->
+		uptime<span><?=$uptime_out;?></span> <!-- Server uptime -->
 		core temp<span><?=$sensors;?></span> <!-- CPU Temperature -->
 		users logged in<span><?=$users_out;?></span> <!-- Users logged in -->
 		load<span><?=$load_out;?></span> <!-- CPU load averages -->
 		memory<span><?=$memory_out;?></span> <!-- RAM usage -->
 		swap<span><?=$swap_out;?></span> <!-- SWAP usage -->
 		disk<span><?=$raiddrive_out;?></span> <!-- RAID information -->
-		<!--  ,------.              ,--.        ,--.                                           ,--.            ,--.  -->
+		<?=($_SERVER['SERVER_NAME'] == "noize.link" ? '' : "<small><small>POWERED BY</small> epic memes</small>");?>
+		<!-- ,------.              ,--.        ,--.                                           ,--.            ,--.   -->
 		<!-- |  .---',--.,--. ,---.|  |,-.     |  | ,---.  ,--,--.,--,--,  ,---.  ,---.  ,---.|  |,-. ,---. ,-'  '-. -->
 		<!-- |  `--, |  ||  || .--'|     /     |  || .-. :' ,-.  ||      \| .-. || .-. || .--'|     /| .-. :'-.  .-' -->
 		<!-- |  |`   '  ''  '\ `--.|  \  \     |  |\   --.\ '-'  ||  ||  || '-' '' '-' '\ `--.|  \  \\   --.  |  |   -->
